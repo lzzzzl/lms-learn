d6b1ed1562754861ce577d846b1c85cc9e8c144d
lzzzzl <lzzzzl005@gmail.com>
2023-10-23 07:41:37 +0800

    add Course Description Form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation";
 import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 
+import { DescriptionForm } from "./_components/description-form";
 import { TitleForm } from "./_components/title-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
@@ -58,6 +59,10 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
             </h2>
           </div>
           <TitleForm 
+            initialData={course}
+            courseId={course.id}
+          />
+          <DescriptionForm 
             initialData={course}
             courseId={course.id}
           />
