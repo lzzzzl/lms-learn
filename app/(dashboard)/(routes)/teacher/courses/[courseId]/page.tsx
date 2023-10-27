97df21b837365a2d5876de53dda9288b0a763e0b
lzzzzl <lzzzzl005@gmail.com>
2023-10-27 21:44:20 +0800

    add course attachment form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,10 +1,11 @@
 import { auth } from "@clerk/nextjs";
-import { CircleDollarSign, LayoutDashboard, ListChecks } from "lucide-react";
+import { CircleDollarSign, File, LayoutDashboard, ListChecks } from "lucide-react";
 import { redirect } from "next/navigation";
 
 import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 
+import { AttachmentForm } from "./_components/attachment-form";
 import { CategoryForm } from "./_components/category-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
@@ -21,6 +22,13 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const course = await db.course.findUnique({
     where: {
       id: params.courseId,
+    },
+    include: {
+      attachments: {
+        orderBy: {
+          createdAt: "desc",
+        },
+      },
     },
   });
 
@@ -108,6 +116,18 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
               </h2>
             </div>
             <PriceForm 
+              initialData={course}
+              courseId={course.id}
+            />
+          </div>
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={File} />
+              <h2 className="text-xl">
+                Resources & Attachments
+              </h2>
+            </div>
+            <AttachmentForm
               initialData={course}
               courseId={course.id}
             />
