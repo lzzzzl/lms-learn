b869fb3be3eff7540d75541fcee6fb9ee5d80f9e
lzzzzl <lzzzzl005@gmail.com>
2023-10-31 23:36:38 +0800

    add chapter description form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -4,6 +4,8 @@ import { auth } from "@clerk/nextjs";
 import { ArrowLeft, LayoutDashboard } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
+
+import { ChapterDescriptionForm } from "./_components/chapter-description-form";
 import { ChapterTitleForm } from "./_components/chapter-title-form";
 
 const ChapterIdPage = async ({
@@ -71,6 +73,11 @@ const ChapterIdPage = async ({
               </h2>
             </div>
             <ChapterTitleForm 
+              initialData={chapter}
+              courseId={params.courseId}
+              chapterId={params.chapterId}
+            />
+            <ChapterDescriptionForm 
               initialData={chapter}
               courseId={params.courseId}
               chapterId={params.chapterId}
